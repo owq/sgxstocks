b84d2bf2f0713a4e7fdaccd651a7a1fe934e35b6
owq <owq@gmx.com>
2015-12-15 22:59:31 +0800

    Switch position of stock code and name

--- app/src/main/java/com/wqlabs/sgxstocks/TradeInfoListAdapter.java
+++ app/src/main/java/com/wqlabs/sgxstocks/TradeInfoListAdapter.java
@@ -77,7 +77,7 @@ public class TradeInfoListAdapter extends BaseExpandableListAdapter {
             String s = String.format(
                     "<b>%s %s</b> %s, %s-%s (%s)" + "<br>" +
                             "(%.0f) %s / %s (%.0f)",
-                    info.getStockCode(), info.getStockName(), info.getLastPrice(), info.getLowPrice(), info.getHighPrice(), info.getOpenPrice(),
+                    info.getStockName(), info.getStockCode(), info.getLastPrice(), info.getLowPrice(), info.getHighPrice(), info.getOpenPrice(),
                     info.getBuyVol(), info.getBuyPrice(), info.getSellPrice(), info.getSellVol());
 
             tv.setText(Html.fromHtml(s));
